3fa64fe17f58f970ff693d2cb1693eb882a19542
Adrian Berenguel <adrianberenguel90@gmail.com>
2019-07-11 21:18:55 +0800

    Installed babel for react and htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: "./src/index.js",
@@ -13,9 +14,12 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-env"]
+          presets: ["@babel/preset-env", "@babel/preset-react"]
         }
       }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/index.html"
+  })]
 }
